29085ac1f8d8ab43142b5e4c59bfef2726e9e918
cauxNicolas <nicaux95@gmail.com>
2020-08-18 12:12:23 +0200

    npm start

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
