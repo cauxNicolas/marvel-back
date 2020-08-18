718cf0a4f1a2ae9631b2c2ad90a13fe8d2d863fc
cauxNicolas <nicaux95@gmail.com>
2020-08-18 15:28:31 +0200

    mongo URI

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(formidable());
 app.use(cors());
 
 // mongoose
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
